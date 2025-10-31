3485f66938e4c3f5261e4cd9dbdb5e5a063743e2
git-mohammedmusthakeem <gitmusthakeem01@gmail.com>
2025-10-31 11:27:37 +0530

    Updated files

--- server/index.ts
+++ server/index.ts
@@ -1,23 +1,19 @@
 import express, { type Request, Response, NextFunction } from "express";
-import http from "http";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
 
-declare module "http" {
+declare module 'http' {
   interface IncomingMessage {
-    rawBody: unknown;
+    rawBody: unknown
   }
 }
-
-app.use(
-  express.json({
-    verify: (req, _res, buf) => {
-      req.rawBody = buf;
-    },
-  })
-);
+app.use(express.json({
+  verify: (req, _res, buf) => {
+    req.rawBody = buf;
+  }
+}));
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
@@ -51,22 +47,35 @@ app.use((req, res, next) => {
 });
 
 (async () => {
-  registerRoutes(app);
+  const server = await registerRoutes(app);
 
   app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
     const status = err.status || err.statusCode || 500;
     const message = err.message || "Internal Server Error";
+
     res.status(status).json({ message });
-    console.error(err);
+    throw err;
   });
 
+  // importantly only setup vite in development and after
+  // setting up all the other routes so the catch-all route
+  // doesn't interfere with the other routes
   if (app.get("env") === "development") {
-    await setupVite(app);
+    await setupVite(app, server);
   } else {
     serveStatic(app);
   }
 
-  const port = parseInt(process.env.PORT || "5000", 10);
-  const server = http.createServer(app);
-  server.listen(port, "0.0.0.0", () => log(`Serving on port ${port}`));
+  // ALWAYS serve the app on the port specified in the environment variable PORT
+  // Other ports are firewalled. Default to 5000 if not specified.
+  // this serves both the API and the client.
+  // It is the only port that is not firewalled.
+  const port = parseInt(process.env.PORT || '5000', 10);
+  server.listen({
+    port,
+    host: "0.0.0.0",
+    reusePort: true,
+  }, () => {
+    log(`serving on port ${port}`);
+  });
 })();
